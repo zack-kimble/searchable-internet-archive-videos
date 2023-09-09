4ad015afe6779549d28a00e0bf2e4bd4a0eb3ea8
Zack Kimble <zkimble@gmail.com>
2023-09-09 13:43:33 -0400

    updates repo push and adds license

--- update_md_repo.sh
+++ update_md_repo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 mkdir -p ~/knox_searchable_meetings_md/meetings
+rm -rf ~/knox_searchable_meetings_md/meetings/*
 cp -rf data/markdown/* ~/knox_searchable_meetings_md/meetings/
 cd ~/knox_searchable_meetings_md/
 git add .
